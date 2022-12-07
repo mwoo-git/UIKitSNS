ff787c45c569407321bbedcb079a969c3cc2a445
WooMin <hanulbom@gmail.com>
2022-12-08 00:57:10 +0900

    Initial Commit

---
+++ UIKitSNS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIKitSNS
+//
+//  Created by Woo Min on 2022/12/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
