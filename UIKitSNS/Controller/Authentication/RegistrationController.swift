41a19f1b71ca2e9773fd5d66c20e9c17d24dea83
mwoo-git <hanulbom@gmail.com>
2023-03-30 02:28:04 +0900

    add: Login 페이지 생성 (1)

---
+++ UIKitSNS/Controller/Authentication/RegistrationController.swift
@@ -0,0 +1,21 @@
+//
+//  RegistrationController.swift
+//  UIKitSNS
+//
+//  Created by Woo Min on 2023/03/30.
+//
+
+import UIKit
+
+class RegistrationController: UIViewController {
+    
+    // MARK: - Properties
+    
+    // MARK: - Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemRed
+    }
+}
